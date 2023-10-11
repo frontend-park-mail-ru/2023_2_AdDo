4d5e6cd0d233b99bf0002d30dd2a08e09d1c9e66
Ambushidozho <d.belyi350@gmail.com>
2023-10-11 20:30:01 +0300

    svg

--- src/components/Player/PlayerTemplate.ts
+++ src/components/Player/PlayerTemplate.ts
@@ -8,11 +8,11 @@ export const source = `<div class="player">
         <div class="progress"></div>
     </div>
     <div class="contentContainer">
-        <div class="prevBtn"><img class="playerBtns" data-section="prevBtn" src="./static/img/SwitchBack2.svg" alt=""></div>
+        <div class="prevBtn"><img class="playerBtns" data-section="prevBtn" src="./static/img/SwitchBack.svg" alt=""></div>
         {{#if Playing}}
         <div class="pauseBtn"><img class="playerBtns"  data-section="pauseBtn"src="./static/img/Pause.svg" alt=""></div>
         {{else}}
-        <div class="playBtn"><img class="playerBtns"  data-section="playBtn"src="./static/img/Play2.svg" alt=""></div>
+        <div class="playBtn"><img class="playerBtns"  data-section="playBtn"src="./static/img/Play.svg" alt=""></div>
         {{/if}}
         {{#each items}}
              <div class="{{this.key}}"><img class="playerBtns" data-section="{{this.key}}" src="{{this.href}}" alt=""></div>
