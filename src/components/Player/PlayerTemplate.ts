1445d337b67cc0f6cfeade46ac2b9dbfa1e8be69
Ambushidozho <d.belyi350@gmail.com>
2023-10-11 19:10:08 +0300

    css

--- src/components/Player/PlayerTemplate.ts
+++ src/components/Player/PlayerTemplate.ts
@@ -11,10 +11,10 @@ export const source = `<div class="player">
         {{#each items}}
              <div class="{{this.key}}"><img class="playerBtns" src="{{this.href}}" alt=""></div>
         {{/each}}
-        <div><img class="avatar" src="{{port}}{{this.Preview}}" alt="./static/img/noImage.png"></div>
+        <div><img class="avatar" src="{{port}}{{song.Preview}}" alt="./static/img/noImage.png"></div>
         <div class="info">
-            <div class="title">{{this.Name}}</div>
-            <div class="artist">{{this.Artist.0.Name}}</div>
+            <div class="title">{{song.Name}}</div>
+            <div class="artist">{{song.Artist.0.Name}}</div>
         </div>
         <div class="likeBtn"><img class="rightPlayerBtns" src="./static/img/likeBtn.svg" alt=""></div>
         <div class="volumeBtn"><img class="rightPlayerBtns" src="./static/img/volumeBtn.svg" alt=""></div>
