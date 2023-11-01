62d509efa8c2e82a1e941fd13a0124cbafd21669
Ambushidozho <d.belyi350@gmail.com>
2023-11-02 01:24:51 +0300

    postcss fix

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
     plugins: [
+      require('postcss-import'),
       require('autoprefixer'),
       require('postcss-nested'),
       require('postcss-short'),
