4c829fee56e63e58d40217531213dfe05f758fe9
Ambushidozho <d.belyi350@gmail.com>
2023-11-08 04:25:14 +0300

    fix

--- src/src-sw.js
+++ src/src-sw.js
@@ -3,12 +3,12 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox
 
 workbox.routing.registerRoute(
     /https:\/\/musicon\.space/,
-    workbox.strategies.networkFirst()
+    workbox.strategies.NetworkFirst()
 );
 
 workbox.routing.registerRoute(
     /https:\/\/api\.s3\.musicon\.space/,
-    workbox.strategies.networkFirst()
+    workbox.strategies.NetworkFirst()
 );
 
 workbox.routing.registerRoute(
