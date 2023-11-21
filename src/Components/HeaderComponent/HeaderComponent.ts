9ed1fab62f2f25bb56ead0d3dc2479fcae109ec0
Ambushidozho <d.belyi350@gmail.com>
2023-11-21 20:54:33 +0300

    css

--- src/Components/HeaderComponent/HeaderComponent.ts
+++ src/Components/HeaderComponent/HeaderComponent.ts
@@ -51,7 +51,7 @@ export class HeaderComponent extends IComponent {
 	private handleSearchClick(e: Event): void {
 		let searchInput: HTMLElement = this.element.querySelector('[data-section="inputSearch"]')!;
 		this.element.querySelectorAll('[data-section="menu-links"]').forEach((element: any) => {
-			element.style.display === 'block'? element.style.display = 'none' : element.style.display = 'block';
+			element.style.display === 'flex'? element.style.display = 'none' : element.style.display = 'flex';
 			
 
 		})
