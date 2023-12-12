1209c274ae052b5dc63b8e2aaa372dbcd7be77f9
Ambushidozho <d.belyi350@gmail.com>
2023-12-12 16:44:42 +0300

    more webpack fix

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,32 @@
+const { merge } = require('webpack-merge')
+
+const commonConfig = require('./webpack.common.js')
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+
+  devServer: {
+    compress: true,
+    open: false,
+    hot: true,
+    port: 8084,
+    client: {
+      progress: false,
+      overlay: {
+        errors: false,
+        warnings: false
+      }
+    },
+    historyApiFallback: true
+  },
+  plugins: [
+
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  watchOptions: {
+    ignored: /node_modules/
+  }
+})
