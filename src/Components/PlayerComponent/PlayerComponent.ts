dc03ad385c35c85bf3748e55e3c82ae260988325
Ambushidozho <d.belyi350@gmail.com>
2023-12-18 21:52:11 +0300

    more fixes

--- src/Components/PlayerComponent/PlayerComponent.ts
+++ src/Components/PlayerComponent/PlayerComponent.ts
@@ -162,15 +162,13 @@ export class PlayerComponent extends IComponent {
 			}
 		});
 		EventDispatcher.subscribe('player-add-track-to-playlist', ({id, type} : { id: string, type: string }) => {
-			if(this.isMounted) {
-				const added = document.querySelector(`[${type}="${id}"]`)! as HTMLElement;
-				added.style.display = 'flex';
-				setTimeout(() => {
-					added.style.display = 'none';
-				}, 2000);
-				const options = document.querySelector(`[data-player="${id}"]`)! as HTMLElement;
-				options.style.display === 'none' ? options.style.display = 'grid' : options.style.display = 'none';
-			}	
+			const added = document.querySelector(`[${type}="${id}"]`)! as HTMLElement;
+			added.style.display = 'flex';
+			setTimeout(() => {
+				added.style.display = 'none';
+			}, 2000);
+			const options = document.querySelector(`[data-player="${id}"]`)! as HTMLElement;
+			options.style.display === 'none' ? options.style.display = 'grid' : options.style.display = 'none';
 		});
 		EventDispatcher.subscribe('mobile-player-show-options', (id: string) => {
 			const options = document.querySelector(`[data-mobile-player="${id}"]`)! as HTMLElement;
@@ -215,15 +213,13 @@ export class PlayerComponent extends IComponent {
 			}
 		});
 		EventDispatcher.subscribe('mobile-player-add-track-to-playlist', ({id, type} : { id: string, type: string }) => {
-			if(this.isMounted) {
-				const added = document.querySelector(`[${type}="${id}"]`)! as HTMLElement;
-				added.style.display = 'flex';
-				setTimeout(() => {
-					added.style.display = 'none';
-				}, 2000);
-				const options = document.querySelector(`[data-mobile-player="${id}"]`)! as HTMLElement;
-				options.style.display === 'none' ? options.style.display = 'grid' : options.style.display = 'none';
-			}	
+			const added = document.querySelector(`[${type}="${id}"]`)! as HTMLElement;
+			added.style.display = 'flex';
+			setTimeout(() => {
+				added.style.display = 'none';
+			}, 2000);
+			const options = document.querySelector(`[data-mobile-player="${id}"]`)! as HTMLElement;
+			options.style.display === 'none' ? options.style.display = 'grid' : options.style.display = 'none';
 		});
 		EventDispatcher.subscribe('copied-to-clipboard', ({id, type} : { id: string, type: string }) => {
 			const copied = document.querySelector(`[${type}="${id}"]`)! as HTMLElement;
