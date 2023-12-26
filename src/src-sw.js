ee6abe3abc029420c506c8fc60cc4189d956cef7
Ambushidozho <d.belyi350@gmail.com>
2023-12-26 21:36:25 +0300

    text break in player

--- src/src-sw.js
+++ src/src-sw.js
@@ -11,11 +11,6 @@ workbox.routing.registerRoute(
 	new NetworkFirst()
 );
 
-// workbox.routing.registerRoute(
-// 	/https:\/\/api\.s3\.musicon\.space/,
-// 	new NetworkFirst()
-// );
-
 workbox.routing.registerRoute(
 	new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
 	new CacheFirst({
